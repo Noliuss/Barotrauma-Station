3dc68c55111983a77f3d4327e75f245e14608e93
Nolius <Noliuss@users.noreply.github.com>
2024-10-03 19:29:46 +0200

    Fix?

--- Content.IntegrationTests/Tests/Preferences/ServerDbSqliteTests.cs
+++ Content.IntegrationTests/Tests/Preferences/ServerDbSqliteTests.cs
@@ -66,7 +66,7 @@ namespace Content.IntegrationTests.Tests.Preferences
                 antagPreferences: new List<string>(),
                 traitPreferences: new List<string>(),
                 loadoutPreferences: new List<string>(),
-                skillPriorities: new List<string>()
+                skillPriority: new List<string>()
             );
         }
 
